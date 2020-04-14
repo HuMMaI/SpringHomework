027163513426d8e8e4904157d43489d2642fa3fc
hummai <shining.melon2015@gmail.com>
2020-04-14 15:47:08 +0300

    Removed scope in student config

--- src/main/java/ua/lviv/lgs/homework/StudentConfig.java
+++ src/main/java/ua/lviv/lgs/homework/StudentConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class StudentConfig {
 
-    @Scope("singleton")
     @Bean(name = "studentDao")
     public StudentDao getStudentDao(){
         return new StudentDao();
